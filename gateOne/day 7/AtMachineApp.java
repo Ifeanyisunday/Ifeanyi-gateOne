2b39036bce114d3e2d569bc39c0739a8c54f6008
Ifeanyisunday <sundayjnr10@gmail.com>
2024-05-13 18:54:06 +0100

    7:00pm

--- gateOne/day 7/AtMachineApp.java
+++ gateOne/day 7/AtMachineApp.java
@@ -2,25 +2,26 @@ import java.util.*;
 public class AtMachineApp{
 	public static void main(String... args){
 
-		/*ArrayList<String>profile2 = new ArrayList<String>();*/
 		Scanner input = new Scanner(System.in);
-		String[] profile1 = new String[3];
-		String[] profile2 = new String[3];
+		ArrayList<String>profile1 = new ArrayList<String>();
+		ArrayList<String>profile2 = new ArrayList<String>();
 		System.out.println("Create your profile");
 		
 		System.out.println("Enter first name:");
 		String user1 = input.next();
-		profile1[0] = user1;
+		profile1.add(user1);
 		System.out.println("Enter last name:");
 		String user2 = input.next();
-		profile1[1] = user2;
+		profile1.add(user2);
 		System.out.println("Enter pin name:");
 		String pin1 = input.next();
-		profile1[2] = pin1;
+		profile1.add(pin1);
 
 		
 
-		System.out.println("Enter first name:");
+		
+
+		/*System.out.println("Enter first name:");
 		String user3 = input.next();
 		profile2[0] = user3;
 		System.out.println("Enter last name:");
@@ -28,55 +29,61 @@ public class AtMachineApp{
 		profile2[1] = user4;
 		System.out.println("Enter pin name:");
 		String pin2 = input.next();
-		profile2[2] = pin2;
+		profile2[2] = pin2;*/
 		
-		int[] accountBalance = new int[2];		
+		int range = 
+		ArrayList<String>accountBalance = new ArrayList<String>();
 		
-		System.out.printf("%s%n%s%n%s%n%s%n", "Choose Transaction type:", "1. Deposit", "2. Withdraw", "3.Transfer");
+		int profile1AccountBalance = accountBalance[0];
+		int profile2AccountBalance = accountBalance[1];		
+		
+		System.out.printf("%s%n%s%n%s%n%s%n%s%n", "Choose Transaction type:", "1. Deposit", "2. Withdraw", "3. Transfer", "4. Check Balance");
 		int index = input.nextInt();
-		int result = transaction(index);
-		
-		System.out.print(result);
-		
+
+		int transferAmt = 0;
+		int transferDone = 0;
 		switch(index){
-			case 3:
-				int transferAmt = input.nextInt();
-				break;
-		}
-
-		
-
-		if(accountBalance[0] == 0){
-			System.out.print("your balance is too low");
-		}else if(accountBalance[0] < transferAmt){
-			System.out.print("your balance is too low");
-		}else{
-			accountBalance[0] - transferAmt;
-		}
-		
-
-		
-
-
-	}
-
-	
-	public static int transaction(int digit){
-		int balance = 0;
-		Scanner input = new Scanner(System.in);
-		switch(digit){
 			case 1:
 				System.out.println("enter deposit amount:");
 				int depositAmt = input.nextInt();
-				balance += depositAmt;
+				profile1AccountBalance += depositAmt;
+				System.out.print(profile1AccountBalance);
 				break;
 			case 2:
 				System.out.println("enter withdrawal amount:");
 				int withdrawAmt = input.nextInt();
-				balance -= withdrawAmt;
+				if(profile1AccountBalance <= 0 && profile1AccountBalance <= withdrawAmt){
+					System.out.print("your balance is too low, Add money to your account");
+				}else{
+					profile1AccountBalance -= withdrawAmt;
+				}
+				System.out.print(profile1AccountBalance);
+				break;
+			case 3:
+				System.out.println("enter transfer amount:");
+				transferAmt = input.nextInt();
+				if(profile1AccountBalance == 0){
+				System.out.print("your balance is too low");
+				}else if(profile1AccountBalance < transferAmt){
+				System.out.print("your balance is too low");
+				}else{
+					transferDone = accountBalance[0] - transferAmt;
+					accountBalance[1] = transferDone;
+					System.out.println(profile2AccountBalance);
+				}
+				break;
+			case 4:
+				System.out.print(profile1AccountBalance);
 				break;
 		}
-		return balance;
+
+		System.out.print(profile1AccountBalance);
+		System.out.print("Do you want to continue? ");
+
+		
+	
+
+
 	}
 
 
