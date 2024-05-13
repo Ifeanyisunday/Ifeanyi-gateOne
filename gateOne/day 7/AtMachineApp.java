e80a1f7ddd083813098b906733346c9bac68719a
Ifeanyisunday <sundayjnr10@gmail.com>
2024-05-13 22:50:58 +0100

    11:00pm

--- gateOne/day 7/AtMachineApp.java
+++ gateOne/day 7/AtMachineApp.java
@@ -3,28 +3,31 @@ public class AtMachineApp{
 	public static void main(String... args){
 
 		Scanner input = new Scanner(System.in);
-		ArrayList<String>accoutFirstName = new ArrayList<String>();
+		/*ArrayList<String>accoutFirstName = new ArrayList<String>();
 		ArrayList<String>accoutLastName = new ArrayList<String>();
-		ArrayList<String>accoutPin = new ArrayList<String>();
+		ArrayList<String>accoutPin = new ArrayList<String>();*/
+
+		ArrayList<ArrayList<String>>account = new ArrayList();
 		System.out.println("Create your profile");
 		
+		String createNew;
 		int count = 0;
 		do{
 			System.out.println("Enter first name:");
-			String user1 = input.next();
-			accoutFirstName.add(user1);
+			String userInput1 = input.next();
+			account.get(0).add(userInput1);
 			System.out.println("Enter last name:");
-			String user2 = input.next();
-			accoutLastName.add(user2);
+			String userInput2 = input.next();
+			account.get(1).add(userInput2);
 			System.out.println("Enter pin name:");
 			String pin1 = input.next();
-			accoutPin.add(pin1);
+			account.get(2).add(pin1);
 			count++;
 			System.out.println("Do you want to create new Account?");
-			String createNew = input.next();
-		while(createNew.equalsIgnoreCase("yes"));
+			createNew = input.next();
+		}while(createNew.equalsIgnoreCase("yes"));
 
-		ArrayList<String>accountBalance = new ArrayList<String>();		
+		ArrayList<Integer>accountBalance = new ArrayList<Integer>();		
 		
 		System.out.printf("%s%n%s%n%s%n%s%n%s%n", "Choose Transaction type:", "1. Deposit", "2. Withdraw", "3. Transfer", "4. Check Balance");
 		int index = input.nextInt();
@@ -33,36 +36,56 @@ public class AtMachineApp{
 		int transferDone = 0;
 		switch(index){
 			case 1:
-				System.out.println("enter deposit amount:");
+				System.out.println("Enter account first name:");
+				String accountName = input.next();
+				System.out.println("Enter deposit amount:");
 				int depositAmt = input.nextInt();
-				accountBalance += depositAmt;
-				System.out.print(profile1AccountBalance);
+				for(int i = 0; i < count; i++){
+					if(accountName == account.get(i).get(i)){
+						accountBalance.add(depositAmt);
+						System.out.print(accountBalance);
+						break;
+					}
+				}
 				break;
 			case 2:
 				System.out.println("enter withdrawal amount:");
+				int mainBalance = 0;
 				int withdrawAmt = input.nextInt();
-				if(profile1AccountBalance <= 0 && profile1AccountBalance <= withdrawAmt){
+				if(accountBalance.get(0) <= 0 && accountBalance.get(0) <= withdrawAmt){
 					System.out.print("your balance is too low, Add money to your account");
 				}else{
-					profile1AccountBalance -= withdrawAmt;
+					for(int j = 0; j < count; j++){
+						if(accountName == account.get(j).get(j)){
+							mainBalance = accountBalance.get(j);
+							mainBalance = mainBalance - withdrawAmt;
+							accountBalance.add(mainBalance);
+							System.out.print(accountBalance[j]);
+						}
+					}
 				}
-				System.out.print(profile1AccountBalance);
 				break;
 			case 3:
+				System.out.println("enter account name:");
+				String enterName = input.next();
 				System.out.println("enter transfer amount:");
 				transferAmt = input.nextInt();
-				if(profile1AccountBalance == 0){
-				System.out.print("your balance is too low");
-				}else if(profile1AccountBalance < transferAmt){
-				System.out.print("your balance is too low");
-				}else{
-					transferDone = accountBalance[0] - transferAmt;
-					accountBalance[1] = transferDone;
-					System.out.println(profile2AccountBalance);
+				for(int a = 0; a < count; a++){
+				if(enterName == account.get(a).get(a)){
+					if(accountBalance.get(a) <= 0){
+						System.out.print("your balance is too low");
+					}else if(accountBalance.get(a) < transferAmt){
+						System.out.print("your balance is too low");
+					}else{
+					transferDone = accountBalance.get(a) - transferAmt;
+					accountBalance.get(1) = transferDone;
+					System.out.println(accountBalance.get(1));
+					}
+				}
 				}
 				break;
 			case 4:
-				System.out.print(profile1AccountBalance);
+				System.out.print(accountBalance.get(1));
 				break;
 		}
 
