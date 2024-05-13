25c8a2d897c10b1352ac0ccef78f82812e98340d
Ifeanyisunday <sundayjnr10@gmail.com>
2024-05-13 16:55:55 +0100

    commit

--- gateOne/day 7/AtMachineApp.java
+++ gateOne/day 7/AtMachineApp.java
@@ -2,20 +2,69 @@ import java.util.*;
 public class AtMachineApp{
 	public static void main(String... args){
 
-		
+		/*ArrayList<String>profile2 = new ArrayList<String>();*/
 		Scanner input = new Scanner(System.in);
+		String[] profile1 = new String[3];
+		String[] profile2 = new String[3];
 		System.out.println("Create your profile");
+		
 		System.out.println("Enter first name:");
-		String name1 = input.next();
+		String user1 = input.next();
+		profile1[0] = user1;
 		System.out.println("Enter last name:");
-		String name2 = input.next();
-		System.out.println("Enter pin:");
-		int pin = input.nextInt();
+		String user2 = input.next();
+		profile1[1] = user2;
+		System.out.println("Enter pin name:");
+		String pin1 = input.next();
+		profile1[2] = pin1;
 
-		int balance = 0;
-		System.out.printf("%s%n%s%n%s%n", "Choose Transaction type:", "1. Deposit", "2. Withdraw");
+		
+
+		System.out.println("Enter first name:");
+		String user3 = input.next();
+		profile2[0] = user3;
+		System.out.println("Enter last name:");
+		String user4 = input.next();
+		profile2[1] = user4;
+		System.out.println("Enter pin name:");
+		String pin2 = input.next();
+		profile2[2] = pin2;
+		
+		int[] accountBalance = new int[2];		
+		
+		System.out.printf("%s%n%s%n%s%n%s%n", "Choose Transaction type:", "1. Deposit", "2. Withdraw", "3.Transfer");
 		int index = input.nextInt();
+		int result = transaction(index);
+		
+		System.out.print(result);
+		
 		switch(index){
+			case 3:
+				int transferAmt = input.nextInt();
+				break;
+		}
+
+		
+
+		if(accountBalance[0] == 0){
+			System.out.print("your balance is too low");
+		}else if(accountBalance[0] < transferAmt){
+			System.out.print("your balance is too low");
+		}else{
+			accountBalance[0] - transferAmt;
+		}
+		
+
+		
+
+
+	}
+
+	
+	public static int transaction(int digit){
+		int balance = 0;
+		Scanner input = new Scanner(System.in);
+		switch(digit){
 			case 1:
 				System.out.println("enter deposit amount:");
 				int depositAmt = input.nextInt();
@@ -27,14 +76,9 @@ public class AtMachineApp{
 				balance -= withdrawAmt;
 				break;
 		}
-		
-
-		System.out.print(balance);
-
-		
-
-
+		return balance;
 	}
+
 
 
 }
