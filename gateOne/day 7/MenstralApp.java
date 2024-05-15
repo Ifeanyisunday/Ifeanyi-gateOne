75b73b06580805d7997e04a52316a9b0a79055b3
Ifeanyisunday <sundayjnr10@gmail.com>
2024-05-15 11:04:21 +0100

    11:00am wednesday

--- gateOne/day 7/MenstralApp.java
+++ gateOne/day 7/MenstralApp.java
@@ -2,7 +2,18 @@ import java.time.*;
 import java.util.*;
 public class MenstralApp{
 	public static void main(String... args){
-	LocalDate current = new LocalDate();
+	Scanner input = new Scanner(System.in);
+	LocalDate LastPeriod = LocalDate.of(2024, 04, 23);
+	LocalDate current = LocalDate.now();
+	Period difference = Period.between(begin, current);
+	int result = difference.getDays();
+
+	System.out.println(result);
+	/*String userName = input.nextLine();
+	String dateOfLastPeriod = input.nextLine();*/
+	
+	
+	
 
 	}
 }
